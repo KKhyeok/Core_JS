e93e30be69e92b2908765e9922c82d6fb748ebab
KKhyeok <js03318@gmail.com>
2023-06-05 19:56:33 +0900

    7-4 요소가 있는 배열을 prototype에 매칭한 경우

--- Chapter.7/7-4.js
+++ Chapter.7/7-4.js
@@ -5,7 +5,7 @@ var Grade = function() {
   }
   this.length = args.length;
 };
-Grade.prototype = [];
+Grade.prototype = ['a', 'b', 'c', 'd'];
 var g = new Grade(100, 80);
 
 g.push(90);
@@ -13,4 +13,4 @@ console.log(g); // Grade { 0: 100, 1: 80, 2: 90, length: 3 }
 
 delete g.length;
 g.push(70);
-console.log(g); // Grade { 0: 70, 1: 80, 2: 90, length: 1 }
+console.log(g); // Grade { 0: 100, 1: 80, 2: 90, ___ 4: 70, length: 5 }
